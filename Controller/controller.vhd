ec1d35b15f5447787516ba601de0646efaf94dda
Keketr <kevin.tranthuan23@gmail.com>
2024-06-03 15:37:46 +0200

    dernier code

--- Controller/controller.vhd
+++ Controller/controller.vhd
@@ -1,21 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Operateur is
-  Port ( op : in std_logic_vector(3 downto 0);
+  Port ( op : in std_logic_vector(2 downto 0);
          SAS : out std_logic;
          SM2, SLU : out std_logic_vector(1 downto 0);
-         SM1 : out std_logic;
-         output : out std_logic
+         SM1 : out std_logic
+         );
 end Operateur;
 
 architecture Structure of Operateur is
@@ -24,7 +16,45 @@ begin
     process(op, SAS, SM2, SLU, SM1)
     begin
         if op ="000" then
-            
-            
+            SAS <= '0';
+            SM1 <= '0';
+            SM2 <= "00";
+            SLU <= NULL;
+        elsif op = "001" then
+            SAS <= '1';
+            SM1 <= '0';
+            SM2 <= "00";
+            SLU <= NULL;
+        elsif op = "010" then
+            SAS <= '0';
+            SM1 <= '1';
+            SM2 <= "00";
+            SLU <= NULL;
+        elsif op = "011" then
+            SAS <= '1';
+            SM1 <= '1';
+            SM2 <= "00";
+            SLU <= NULL;
+        elsif op = "100" then
+            SAS <= NULL;
+            SM1 <= NULL;
+            SM2 <= "01";
+            SLU <= NULL;
+        elsif op = "101" then
+            SAS <= NULL;
+            SM1 <= NULL;
+            SM2 <= "10";
+            SLU <= "01";
+        elsif op = "110" then
+            SAS <= NULL;
+            SM1 <= NULL;
+            SM2 <= "10";
+            SLU <= "10";
+        elsif op = "110" then
+            SAS <= NULL;
+            SM1 <= NULL;
+            SM2 <= "10";
+            SLU <= "11";
+    end process;
 
 end Structure;
